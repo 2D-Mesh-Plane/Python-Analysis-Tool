cd35ee8fd74335fe65b3e5bdc9edef8372eadcf8
Madmegsox <madmegsox2@gmail.com>
2021-12-25 10:31:48 +0000

    [added] project files

---
+++ src/main/kotlin/org/madmeg/api/Test.kt
@@ -0,0 +1,8 @@
+package org.madmeg.api
+
+/**
+ * @author Madmegsox1
+ * @since 25/12/2021
+ */
+
+
---
+++ src/main/kotlin/org/madmeg/impl/Core.kt
@@ -0,0 +1,11 @@
+package org.madmeg.impl
+
+/**
+ * @author Madmegsox1
+ * @since 25/12/2021
+ */
+
+
+fun main(){
+    
+}
